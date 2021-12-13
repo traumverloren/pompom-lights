2d0653ab675e8dbd0d0b82994e2b5f732dfde4cd
Stephanie Nemeth <traumverloren@gmail.com>
2021-12-13 19:13:09 +0100

    hue works!

--- ble_client.py
+++ ble_client.py
@@ -1,25 +1,28 @@
-import adafruit_requests as requests
-
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
+import socket
+import adafruit_requests
+
+http = adafruit_requests.Session(socket)
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
 # source control.
-# pylint: disable=no-name-in-module,wrong-import-order
 try:
     from secrets import secrets
 except ImportError:
     print("api secrets are kept in secrets.py, please add them there!")
     raise
 
+
 ble = BLERadio()
 uart_connection = None
 
+HUE_URL = 'http://' + secrets["ip_address"] + '/api/' + secrets["api_id"] + '/groups/12/action'
+
 color = ''
-new_color = ''
 
 while True:
     if not uart_connection:
@@ -34,9 +37,13 @@ while True:
     if uart_connection and uart_connection.connected:
         uart_service = uart_connection[UARTService]
         while uart_connection.connected:
-            new_color = uart_service.readline().decode("utf-8")
-            if new_color != color:
-                color = new_color
-                print(current_color)
-                if color == "green":
-                    requests.put('https://192.168.178.41/api/' + {secrets["api_id"]} + '/groups/12/action', {"scene":"URnH9in2-faN3hS"})
+            print(uart_service.readline().decode("utf-8"))
+            if "green" in uart_service.readline().decode("utf-8"):
+                print("COLOR IS GREEN!!!!")
+                print("PUTing data to {0}".format(HUE_URL))
+                response = http.put(HUE_URL, data='{"scene":"URnH9in2-faN3hS"}')
+
+                print("-" * 40)
+                print(response.status_code)
+                print("-" * 40)
+                response.close()
