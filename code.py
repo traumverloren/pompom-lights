7795eaabf48dcbdf584b01ca7d3b5a5fb37c7f18
Stephanie Nemeth <stephanie@hey.com>
2021-12-18 21:46:35 +0100

    increase touch sensitivity threshold

--- code.py
+++ code.py
@@ -27,6 +27,11 @@ touch_A3 = touchio.TouchIn(board.A3)  # on/off
 touch_A4 = touchio.TouchIn(board.A4)  # green
 touch_A6 = touchio.TouchIn(board.A6)  # blue
 
+touch_sensors = [touch_A1, touch_A3, touch_A4, touch_A6]
+
+for sensor in touch_sensors:
+    sensor.threshold = 1500
+
 pixels = neopixel.NeoPixel(
     board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
 
@@ -41,6 +46,7 @@ OFF = (0, 0, 0)
 
 new_color = ""
 current_color = "on"
+is_touched = False
 
 while True:
     ble.start_advertising(advertisement)
@@ -49,58 +55,62 @@ while True:
         pass
     print("Connected")
     while ble.connected:
-        time.sleep(1.5)
+        if touch_A1 or touch_A3 or touch_A4 or touch_A6:
+            is_touched = True
+            time.sleep(1)
 
-        if touch_A3.value:
-            print("A3 touched!")
-            if current_color == "off" or current_color == "":
-                pixels.fill(WHITE)
+        if is_touched:
+            if touch_A3.value:
+                print("A3 touched!")
+                if current_color == "off" or current_color == "":
+                    pixels.fill(WHITE)
+                    pixels.show()
+                    new_color = "on"
+                else:
+                    new_color = "off"
+
+            elif touch_A1.value and touch_A6.value and current_color != "yellow":
+                print("Touched red & green!")
+                pixels.fill(YELLOW)
                 pixels.show()
-                new_color = "on"
-            else:
-                new_color = "off"
+                new_color = "yellow"
 
-        elif touch_A1.value and touch_A6.value and current_color != "yellow":
-            print("Touched red & green!")
-            pixels.fill(YELLOW)
-            pixels.show()
-            new_color = "yellow"
+            elif touch_A1.value and touch_A4.value and current_color != "purple":
+                print("Touched red & blue!")
+                pixels.fill(PURPLE)
+                pixels.show()
+                new_color = "purple"
 
-        elif touch_A1.value and touch_A4.value and current_color != "purple":
-            print("Touched red & blue!")
-            pixels.fill(PURPLE)
-            pixels.show()
-            new_color = "purple"
+            elif touch_A4.value and touch_A6.value and current_color != "cyan":
+                print("Touched blue & green!")
+                pixels.fill(CYAN)
+                pixels.show()
+                new_color = "cyan"
 
-        elif touch_A4.value and touch_A6.value and current_color != "cyan":
-            print("Touched blue & green!")
-            pixels.fill(CYAN)
-            pixels.show()
-            new_color = "cyan"
+            elif touch_A1.value and current_color != "red":
+                print("Touched A1!")
+                pixels.fill(RED)
+                pixels.show()
+                new_color = "red"
 
-        elif touch_A1.value and current_color != "red":
-            print("Touched A1!")
-            pixels.fill(RED)
-            pixels.show()
-            new_color = "red"
+            elif touch_A4.value and current_color != "blue":
+                print("Touched A4!")
+                pixels.fill(BLUE)
+                pixels.show()
+                new_color = "blue"
 
-        elif touch_A4.value and current_color != "blue":
-            print("Touched A4!")
-            pixels.fill(BLUE)
-            pixels.show()
-            new_color = "blue"
+            elif touch_A6.value and current_color != "green":
+                print("Touched A6!")
+                pixels.fill(GREEN)
+                pixels.show()
+                new_color = "green"
 
-        elif touch_A6.value and current_color != "green":
-            print("Touched A6!")
-            pixels.fill(GREEN)
-            pixels.show()
-            new_color = "green"
-
-        if current_color != new_color:
-            current_color = new_color
-            print(new_color)
-            data = current_color + "\n"
-            uart.write(data.encode("utf-8"))
-            time.sleep(2)
-            pixels.fill(OFF)
-            pixels.show()
+            if current_color != new_color:
+                is_touched = False
+                current_color = new_color
+                print(new_color)
+                data = current_color + "\n"
+                uart.write(data.encode("utf-8"))
+                time.sleep(1)
+                pixels.fill(OFF)
+                pixels.show()
